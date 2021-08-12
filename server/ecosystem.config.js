4ab8e50d6d5b9a44111190575f520d30356f2fe0
James Hrivnak <jameshrivnak4@gmail.com>
2021-08-12 01:16:45 -0600

    making the key route run from root

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
     production: {
       user: 'ubuntu',
       host: 'ec2-3-15-110-200.us-east-2.compute.amazonaws.com',
-      key: '../Login_Scripts/personalwebsite.pem',
+      key: './Login_Scripts/personalwebsite.pem',
       ref: 'origin/main',
       repo: 'git@github.com:jamesh48/PersonalWebsite.git',
       path: '/home/ubuntu/PersonalWebsite',
