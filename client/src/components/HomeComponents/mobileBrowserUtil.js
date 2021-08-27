ba9abbeac3edb6a53ce80930bf9a958af55a1847
James Hrivnak <jameshrivnak4@gmail.com>
2021-08-26 19:14:10 -0600

    handled for the browser not detecting an ipad properly

--- client/src/components/HomeComponents/mobileBrowserUtil.js
+++ client/src/components/HomeComponents/mobileBrowserUtil.js
@@ -1,15 +1,16 @@
 export default () => {
   const toMatch = [
-      /Android/i,
-      /webOS/i,
-      /iPhone/i,
-      /iPad/i,
-      /iPod/i,
-      /BlackBerry/i,
-      /Windows Phone/i
+    /Android/i,
+    /webOS/i,
+    /iPhone/i,
+    /iPad/i,
+    /iPod/i,
+    /BlackBerry/i,
+    /Windows Phone/i
   ];
 
   return toMatch.some((toMatchItem) => {
-      return navigator.userAgent.match(toMatchItem);
+    // Second condition works for ipads that display intel mac...
+    return navigator.userAgent.match(toMatchItem) || (navigator.userAgent.indexOf('Macintosh') > -1 && 'ontouchend' in document);
   });
 }
