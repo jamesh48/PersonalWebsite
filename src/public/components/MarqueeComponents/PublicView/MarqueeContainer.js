46df926286a1ed60587618f52bf95a1b119d280a
James Hrivnak <James.Hrivnak@united.com>
2023-10-31 21:07:54 -0600

    Upgrade version and test deployment on new computer

--- src/public/components/MarqueeComponents/PublicView/MarqueeContainer.js
+++ src/public/components/MarqueeComponents/PublicView/MarqueeContainer.js
@@ -42,25 +42,17 @@ export const MarqueeContainer = ({ smileCallback }) => {
     handleSmileImage({ url: `${cFLink}/main/main-images/linkedin.jpg` });
   }, [portrait]);
 
-  const paragraphOne = `I have just finished working on several software engineering applications- my favorite technologies are front end technologies like React and its testing frameworks, but I particularly enjoy working on optimizing backend Relational Databases like mySQL and Postgres. For example, I recently worked on horizontally scaling an Amazon Web Services microservice to take in a large RPS which mainly required optimizing database queries. I really enjoy coding- particularly working on teams while also working solo.`;
+  const paragraphOne = `I have worked as an AWS Devops Engineer for the past two years. I am skilled in various services that AWS offers including EKS, ECS, ActiveMQ, Lambda/Serverless, CDK, and DynamoDB, among many others. My experience in this role has also equipped me with expertise in CI/CD pipelines, Docker, and Kubernetes. From a application development perspective, I also have a wealth of Fullstack Experience using Node, React, and Typescript.`;
 
-  const paragraphTwo = `I am kind, compassionate, empathetic and a good listener. I have a background in secular Buddhism, and have worked on compassionate and mindful teams in eldercare for the past two years. I value self care but I also like going in hard. I’ve trained for and have run a marathon, and I was also a competitive swimmer in college and now. I also have lived in two foreign countries and have visited 32 others.`;
+  const paragraphTwo = `I am a highly motivated and detail-oriented professional, always striving for excellence in my work. I am adept at problem-solving and have a strong ability to analyze complex requirements and translate them into practical and efficient solutions. Additionally, my leadership experience in coordinating deployments and guiding frontend and backend developers further highlights my ability to work collaboratively and effectively in a team environment.`;
 
   // Load Portrait mode if its a mobileBrowser and is in portrait orientation
   // OR--
   // Load Portrait mode if its a browser and a small window
   return smileImage.loaded && ((mobileBrowser && portrait) || (!mobileBrowser && smallWindow)) ? (
-    <MCPortrait
-      smileImage={smileImage}
-      paragraphOne={paragraphOne}
-      paragraphTwo={paragraphTwo}
-    />
+    <MCPortrait smileImage={smileImage} paragraphOne={paragraphOne} paragraphTwo={paragraphTwo} />
   ) : smileImage.loaded ? (
-    <MCLandscape
-      smileImage={smileImage}
-      paragraphOne={paragraphOne}
-      paragraphTwo={paragraphTwo}
-    />
+    <MCLandscape smileImage={smileImage} paragraphOne={paragraphOne} paragraphTwo={paragraphTwo} />
   ) : (
     <p>Loading Marquee Container....</p>
   );
--- todo.md
+++ todo.md
@@ -1,3 +1,11 @@
 [ ] amazon s3 sdk for bucket policy update
 [ ] learn how to use certain versions of node with a script or program
 [ ] Finish Portfolio Admin UI (Github links) with FullStack Integration.
+
+const paragraphOne = `I have just finished working on several software engineering applications- my favorite technologies are front end technologies like React and its testing frameworks, but I particularly enjoy working on optimizing backend Relational Databases like mySQL and Postgres. For example, I recently worked on horizontally scaling an Amazon Web Services microservice to take in a large RPS which mainly required optimizing database queries. I really enjoy coding- particularly working on teams while also working solo.`;
+
+const paragraphTwo = `I am kind, compassionate, empathetic and a good listener. I have a background in secular Buddhism, and have worked on compassionate and mindful teams in eldercare for the past two years. I value self care but I also like going in hard. I’ve trained for and have run a marathon, and I was also a competitive swimmer in college and now. I also have lived in two foreign countries and have visited 32 others.`;
+
+const paragraphOne = `I have worked as an AWS Devops Engineer for the past two years. I am skilled in various services that AWS offers including EKS, ECS, ActiveMQ, Lambda/Serverless, CDK, and DynamoDB, among many others. My experience in this role has also equipped me with expertise in CI/CD pipelines, Docker, and Kubernetes. From a application development perspective, I also have a wealth of Fullstack Experience using Node, React, and Typescript.`;
+
+const paragraphTwo = `I am a highly motivated and detail-oriented professional, always striving for excellence in my work. I am adept at problem-solving and have a strong ability to analyze complex requirements and translate them into practical and efficient solutions. Additionally, my leadership experience in coordinating deployments and guiding frontend and backend developers further highlights my ability to work collaboratively and effectively in a team environment.`;
